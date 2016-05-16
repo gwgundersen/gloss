b7e4f0ac3d11be9ed0aee265955e8ffdff7ba0a4
gwg <ggundersen@gmail.com>
2016-05-16 08:00:10 -0400

    Render glosses by type.

--- glossary/endpoints/gloss.py
+++ glossary/endpoints/gloss.py
@@ -14,7 +14,7 @@ gloss_blueprint = Blueprint('gloss',
 
 @gloss_blueprint.route('/<int:gloss_id>', methods=['GET'])
 def render_gloss(gloss_id):
-    """Render glossary."""
+    """Render gloss by ID."""
     gloss = db.session.query(models.Gloss).get(gloss_id)
     pdoc_args = ['--mathjax', '--smart']
     output = pypandoc.convert(gloss.text_,
@@ -23,3 +23,10 @@ def render_gloss(gloss_id):
                               extra_args=pdoc_args)
     return render_template('gloss.html',
                            rendered_gloss=output)
+
+
+@gloss_blueprint.route('/<string:gloss_type>', methods=['GET'])
+def render_gloss_type(gloss_type):
+    """Render all glosses of a type."""
+    glosses = db.session.query(models.Gloss).filter_by(type_=gloss_type).all()
+    return render_template('glosses.html', glosses=glosses)
