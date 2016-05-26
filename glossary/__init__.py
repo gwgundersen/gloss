d379ae1c8981b5e982a4109fa7104ebc0a0d871b
gwg <ggundersen@gmail.com>
2016-05-26 08:59:03 -0400

    Pandoc configuration and error handling.

--- glossary/__init__.py
+++ glossary/__init__.py
@@ -97,4 +97,6 @@ def method_not_allowed(e):
 # Set binary location for pandoc. This is important because Flask and Apache
 # both run as special users and cannot find the binaries otherwise.
 # ----------------------------------------------------------------------------
-os.environ.setdefault("PYPANDOC_PANDOC", config.get('general', 'pandoc_path'))
+os.environ.setdefault("PYPANDOC_PANDOC", config.get('path', 'pandoc'))
+if not config.get('mode', 'debug'):
+    os.environ.setdefault("LD_LIBRARY_PATH", config.get('path', 'ld_library'))
