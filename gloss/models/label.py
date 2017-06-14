8d3834e8c0d88d04c8e1561870d9b1a157ec7aa3
Greg <ggundersen@gmail.com>
2017-06-14 18:17:58 -0400

    Label descriptions.

--- gloss/models/label.py
+++ gloss/models/label.py
@@ -8,6 +8,7 @@ class Label(db.Model):
     __tablename__ = 'label'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
+    description = db.Column(db.String(255))
     endpoint = 'label'
 
     @property
