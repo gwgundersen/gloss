f2dd39d310ce87f153ab9ee7577d2b7d1c158780
Greg <ggundersen@gmail.com>
2017-06-15 11:05:32 -0400

    Created searchengine module and improved search functionality to search through author names, entity titles, and gloss text.

--- gloss/endpoints/index.py
+++ gloss/endpoints/index.py
@@ -1,10 +1,9 @@
 """Render landing page."""
 
-from sqlalchemy import text
 from flask import Blueprint, render_template, request
 from flask.ext.login import login_required
 
-from gloss import db, models
+from gloss import db, models, searchengine
 from gloss.config import config
 
 
@@ -44,7 +43,7 @@ def render_index_page():
             .all()
         labels = db.session.query(models.Label).all()
     else:
-        glosses = _get_glosses_by_keyword(keyword)
+        glosses = searchengine.find_glosses_by_keyword(keyword)
 
     if len(glosses) > MAX_PER_PAGE:
         c_prev = max(0, cursor - MAX_PER_PAGE)
@@ -57,16 +56,3 @@ def render_index_page():
         c_prev = c_next = None
     return render_template('index.html', glosses=glosses, labels=labels,
                            q=keyword, c_prev=c_prev, c_next=c_next)
-
-
-def _get_glosses_by_keyword(keyword):
-    """Return glosses based on keyword matches in text."""
-    sql = 'SELECT id FROM gloss WHERE MATCH (text_) AGAINST (:keyword)'
-    conn = db.engine.connect()
-    t = conn.execute(text(sql), keyword=keyword)
-    ids = [int(g[0]) for g in t]
-    glosses = db.session.query(models.Gloss)\
-        .filter(models.Gloss.id.in_(ids))\
-        .order_by(models.Gloss.timestamp.desc())\
-        .all()
-    return glosses
---
+++ gloss/searchengine/__init__.py
@@ -0,0 +1,94 @@
+"""Search engine for glosses.
+"""
+
+from sqlalchemy import text
+
+from gloss import db, models
+
+
+def find_glosses_by_keyword(keyword):
+    """Return glosses based on keyword matches."""
+    ids = []
+    ids += _find_gloss_ids_by_text(keyword)
+    ids += _find_gloss_ids_by_author(keyword)
+    ids += _find_gloss_ids_by_titles(keyword)
+    # Make list of IDs unique to prevent duplicates.
+    ids = set(ids)
+    glosses = db.session.query(models.Gloss)\
+        .filter(models.Gloss.id.in_(ids))\
+        .order_by(models.Gloss.timestamp.desc())\
+        .all()
+    return glosses
+
+
+def _find_gloss_ids_by_text(keyword):
+    """Return gloss IDs based on keyword matches in text.
+    """
+    sql = 'SELECT id FROM gloss WHERE MATCH (text_) AGAINST (:keyword)'
+    conn = db.engine.connect()
+    t = conn.execute(text(sql), keyword=keyword)
+    return [int(g[0]) for g in t]
+
+
+def _find_gloss_ids_by_titles(keyword):
+    """Return gloss IDs based on keyword matches in entity titles.
+    """
+    ids = []
+
+    sql = 'SELECT gloss.id FROM gloss '\
+          '  JOIN book ON book.entity_fk = gloss.entity_fk '\
+          'WHERE MATCH (book.title) AGAINST (:keyword)'
+    conn = db.engine.connect()
+    t = conn.execute(text(sql), keyword=keyword)
+    ids += [int(g[0]) for g in t]
+
+    sql = 'SELECT gloss.id FROM gloss '\
+          '  JOIN paper ON paper.entity_fk = gloss.entity_fk '\
+          'WHERE MATCH (paper.title) AGAINST (:keyword)'
+    conn = db.engine.connect()
+    t = conn.execute(text(sql), keyword=keyword)
+    ids += [int(g[0]) for g in t]
+
+    sql = 'SELECT gloss.id FROM gloss '\
+          '  JOIN talk ON talk.entity_fk = gloss.entity_fk '\
+          'WHERE MATCH (talk.title) AGAINST (:keyword)'
+    conn = db.engine.connect()
+    t = conn.execute(text(sql), keyword=keyword)
+    ids += [int(g[0]) for g in t]
+
+    return ids
+
+
+def _find_gloss_ids_by_author(keyword):
+    """Return gloss IDs based on keyword matches in text.
+    """
+    ids = []
+
+    # Find authors of books.
+    sql = 'SELECT gloss.id FROM gloss '\
+          '  JOIN author_to_book ON author_to_book.book_fk = gloss.entity_fk '\
+          '  JOIN author ON author.id = author_to_book.author_fk '\
+          'WHERE MATCH (author.name) AGAINST (:keyword)'
+    conn = db.engine.connect()
+    t = conn.execute(text(sql), keyword=keyword)
+    ids += [int(g[0]) for g in t]
+
+    # Find authors of papers.
+    sql = 'SELECT gloss.id FROM gloss '\
+          '  JOIN author_to_paper ON author_to_paper.paper_fk = gloss.entity_fk '\
+          '  JOIN author ON author.id = author_to_paper.author_fk '\
+          'WHERE MATCH (author.name) AGAINST (:keyword)'
+    conn = db.engine.connect()
+    t = conn.execute(text(sql), keyword=keyword)
+    ids += [int(g[0]) for g in t]
+
+    # Find authors of talks.
+    sql = 'SELECT gloss.id FROM gloss '\
+          '  JOIN author_to_talk ON author_to_talk.talk_fk = gloss.entity_fk '\
+          '  JOIN author ON author.id = author_to_talk.author_fk '\
+          'WHERE MATCH (author.name) AGAINST (:keyword)'
+    conn = db.engine.connect()
+    t = conn.execute(text(sql), keyword=keyword)
+    ids += [int(g[0]) for g in t]
+
+    return ids
